a938e3d681eb2b413347419231e55e5da1f2047f
muralimano <muralimanoharsoft@gmail.com>
2018-11-22 10:39:52 +0530

    Backend server configured and build gradle upgraded

--- app/src/main/java/com/oceansoftwares/store/models/banner_model/BannerDetails.java
+++ app/src/main/java/com/oceansoftwares/store/models/banner_model/BannerDetails.java
@@ -4,7 +4,7 @@ import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
 
-public class BannerDetails {
+public class    BannerDetails {
 
     @SerializedName("id")
     @Expose
